e714960c9fc3211152232c43f87c1aa861ca37d2
tronghh <tronghh@ait.com>
2023-08-17 16:29:34 +0700

    complete

--- app/src/main/java/com/example/thirtydaysapp/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysapp/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.thirtydaysapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
@@ -24,15 +33,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.thirtydaysapp.model.AppContentModel
 import com.example.thirtydaysapp.ui.theme.ThirtyDaysAppTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.thirtydaysapp.data.AppContent
 
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -49,14 +67,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun AppTag(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @ExperimentalMaterial3Api
 @Composable
 fun App() {
@@ -64,15 +74,14 @@ fun App() {
         topBar = {
             AppTopBar()
         }
-    ) { it ->
+    ) {
         LazyColumn(
-            modifier = modifier,
+            modifier = Modifier,
             contentPadding = it
         ) {
             items(AppContent().loadAppContent()) { appCard ->
                 AppCard(
                     appContentModel = appCard,
-                    modifier = Modifier.padding(8.dp)
                 )
             }
         }
@@ -85,10 +94,12 @@ fun App() {
 fun AppTopBar(modifier: Modifier = Modifier){
     CenterAlignedTopAppBar(
         title = {
-            Row() {
+            Row{
                 Text(
                     text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.displayLarge
+                    style = MaterialTheme.typography.displayMedium,
+                    fontWeight = FontWeight.Bold,
+
                 )
             }
         },
@@ -98,13 +109,48 @@ fun AppTopBar(modifier: Modifier = Modifier){
 
 @Composable
 fun AppCard(appContentModel: AppContentModel, modifier: Modifier = Modifier){
+    var expanded by remember { mutableStateOf(false) }
     Card (
         modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp)
     ){
         Column (
             modifier = Modifier
-                .padding(10.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
         ){
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                Text(
+                    text = LocalContext.current.getString(appContentModel.titleResourceId),
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                AppCardButton(expanded = expanded, onClick = { expanded = !expanded })
+            }
+            Text(
+                text = LocalContext.current.getString(appContentModel.contentResourceId),
+                modifier = Modifier.padding(bottom = 16.dp, start = 16.dp, end = 16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            if (expanded) {
+                AppExpand(
+                    appDetail = R.string.hiddenContent,
+                    modifier = Modifier.padding(bottom = 16.dp, start = 16.dp, end = 16.dp),
+
+                    )
+            }
             Image(
                 painter = painterResource(appContentModel.imageResourceId),
                 contentDescription = stringResource(appContentModel.titleResourceId),
@@ -113,32 +159,46 @@ fun AppCard(appContentModel: AppContentModel, modifier: Modifier = Modifier){
                     .height(194.dp),
                 contentScale = ContentScale.Crop
             )
-            Text(
-                text = LocalContext.current.getString(appContentModel.titleResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
-            Text(
-                text = LocalContext.current.getString(appContentModel.contentResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+private fun AppCardButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
         modifier = modifier
-    )
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+            contentDescription = "Expand",
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
 }
 
+@Composable
+fun AppExpand(@StringRes appDetail: Int, modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = appDetail),
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
+
+}
+
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun AppPreview() {
-    ThirtyDaysAppTheme {
+    ThirtyDaysAppTheme(darkTheme = false) {
         App()
     }
 }
--- app/src/main/java/com/example/thirtydaysapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/thirtydaysapp/ui/theme/Theme.kt
@@ -1,13 +1,22 @@
 package com.example.thirtydaysapp.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
-private val LightColors = lightColorScheme(
+private val lightColors = lightColorScheme(
     primary = md_theme_light_primary,
     onPrimary = md_theme_light_onPrimary,
     primaryContainer = md_theme_light_primaryContainer,
@@ -40,7 +49,7 @@ private val LightColors = lightColorScheme(
 )
 
 
-private val DarkColors = darkColorScheme(
+private val darkColors = darkColorScheme(
     primary = md_theme_dark_primary,
     onPrimary = md_theme_dark_onPrimary,
     primaryContainer = md_theme_dark_primaryContainer,
@@ -74,17 +83,31 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun ThirtyDaysAppTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  dynamicColor: Boolean = false,
   content: @Composable() () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
-  MaterialTheme(
-    colorScheme = colors,
-    content = content
-  )
+        darkTheme -> darkColors
+        else -> lightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
